d5bb20a11ad46423411693e68a52f4419580669e
Stephanie Wong <stephw.yw@gmail.com>
2016-08-02 15:15:14 +0800

    user login stable

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,21 +1,22 @@
 class SessionsController < ApplicationController
-
   def new
   end
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      log_in user
+      flash[:success] = 'Login successful'
+      log_in(user)
       redirect_to user
     else
-      flash.now[:danger] = 'Invalid email/password combination'
+      flash.now[:danger] = 'Invalid email/password combination.'
       render 'new'
     end
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
+    flash[:info] = 'Logout successful.'
     redirect_to root_url
   end
 
